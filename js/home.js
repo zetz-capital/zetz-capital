fdcbb1c26586dff8d781f1712ec57443fb55d40e
TuriHF <turihf@gmail.com>
2026-07-15 17:09:50 -0300

    Add files via upload

--- js/home.js
+++ js/home.js
@@ -1,1 +1,13 @@
+document.getElementById("current-year").textContent = new Date().getFullYear();
 
+document.querySelectorAll(".disabled-link").forEach((link) => {
+  link.addEventListener("click", (event) => event.preventDefault());
+});
+
+document.querySelectorAll("[data-event]").forEach((link) => {
+  link.addEventListener("click", () => {
+    if (typeof gtag === "function") {
+      gtag("event", link.dataset.event);
+    }
+  });
+});
